1154b1e871628d24d67dfbb70c73ece4adbb958c
aprildykj <aprildykj@gmail.com>
2020-09-12 22:57:52 +0800

    :seedling: vinavr begining...

---
+++ task/core/main_mission.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: main_mission.cpp
+	> Author: april
+	> Mail: aprildykj@gmail.com 
+	> Created Time: Sat, Sep 12, 2020 10:55:50 PM
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+>>主线：
+>>信工->清华->创业->全球
+>>
